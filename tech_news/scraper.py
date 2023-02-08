2286816b1bf5cdbb134e5180e4f0bfa3001002d8
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-02-08 19:52:16 -0300

    feat: implementando scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+import re
 
 
 # Requisito 1
@@ -42,9 +43,41 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    response_text = html_content
+    path_url = "head > link[rel=canonical]::attr(href)"
+    path_title = "h1::text"
+    path_timestamp = "ul > li.meta-date::text"
+    path_writer = "ul > li.meta-author > span.author > a::text"
+    path_reading_time = "ul > li.meta-reading-time::text"
+    path_summary = "div.entry-content > p:nth-of-type(1) *::text"
+    path_category = "div > a > span.label::text"
+    selector = Selector(text=response_text)
+    url = selector.css(path_url).get()
+    title = selector.css(path_title).get()
+    timestamp = selector.css(path_timestamp).get()
+    writer = selector.css(path_writer).get()
+    reading_time = selector.css(path_reading_time).get()
+    summary = selector.css(path_summary).getall()
+    category = selector.css(path_category).get()
+    dict_page = {
+        "url": url,
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(''.join(re.findall(r"\d", reading_time)).strip()),
+        "summary": ''.join(summary).strip(),
+        "category": category,
+    }
+    print(dict_page)
+    return dict_page
 
 
-# Requisito 5
+if __name__ == '__main__':
+    scrape_news(
+        fetch("https://blog.betrybe.com/noticias/bill-gates-e-cetico-sobre-criptomoedas-e-nfts-entenda-o-motivo/")
+    )
+
+
+#requisito5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
