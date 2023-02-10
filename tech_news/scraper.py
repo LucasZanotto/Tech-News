50302d669f6603a97929104aa05ad4b512e74a08
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-02-10 19:01:57 -0300

    feat: implementando a função get_tech_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import requests
 import time
 from parsel import Selector
 import re
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -64,20 +65,46 @@ def scrape_news(html_content):
         "title": title.strip(),
         "timestamp": timestamp,
         "writer": writer,
-        "reading_time": int(''.join(re.findall(r"\d", reading_time)).strip()),
-        "summary": ''.join(summary).strip(),
+        "reading_time": int("".join(re.findall(r"\d", reading_time)).strip()),
+        "summary": "".join(summary).strip(),
         "category": category,
     }
-    print(dict_page)
     return dict_page
-
-
-# if __name__ == '__main__':
-#     scrape_news(
-#         fetch("https://blog.betrybe.com/noticias/bill-gates-e-cetico-sobre-criptomoedas-e-nfts-entenda-o-motivo/")
-#     )
 
 
 # requisito5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    count_notices = 0
+    next_count = 0
+    list_dict = []
+    next_html = ''
+    html_content = fetch("https://blog.betrybe.com/")
+    page_notices = scrape_updates(html_content)
+    count_notices += len(page_notices)
+    next_page = scrape_next_page_link(html_content)
+    for index in range(0, amount):
+        if index <= 11:
+            dict_notices = scrape_news(fetch(page_notices[index]))
+            list_dict.append(dict_notices)
+        if index >= 12:
+            if next_count >= 12:
+                next_count = 0
+                next_page = scrape_next_page_link(next_html)
+            next_html = fetch(next_page)
+            # print(next_html)
+            next_notices = scrape_updates(next_html)
+            dict_notices = scrape_news(fetch(next_notices[next_count]))
+            list_dict.append(dict_notices)
+            next_count += 1
+
+    # print(len(list_dict[:amount]))
+    create_news(list_dict[:amount])
+    return list_dict[:amount]
+
+
+if __name__ == "__main__":
+    # print(scrape_updates(fetch('https://blog.betrybe.com/')))
+    # scrape_news(
+    #     fetch("https://blog.betrybe.com/noticias/bill-gates-e-cetico-sobre-criptomoedas-e-nfts-entenda-o-motivo/")
+    # )
+    get_tech_news(33)
