28b7013cd1522f4ca59ef546d67135fc090d3071
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-02-14 13:28:33 -0300

    feat: implementando os testes

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,66 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import ReadingPlanService
+from unittest.mock import patch
+import pytest
+
+
+def mock_list():
+    mock1 = {
+        "_id": {"$oid": "63ea68e9ee4750d72db4df0e"},
+        "url": "https://blog.betrybe.com/tecnologia/exemplos-de-algoritmos/",
+        "title": "computação",
+        "timestamp": "03/02/2023",
+        "writer": "Lucas Custódio",
+        "reading_time": 5,
+        "summary": "Quando falamos de algoritmos, pensamos em matemática.",
+        "category": "Tecnologia",
+    }
+    mock2 = {
+        "_id": {"$oid": "63ea68e9ee4750d72db4df0f"},
+        "url": "https://blog.betrybe.com/carreira/trybetalks-gaules/",
+        "title": "TrybeTalks — Gaules",
+        "timestamp": "30/01/2023",
+        "writer": "Lucas Custódio",
+        "reading_time": 9,
+        "summary": "Gaules divide um pouco de sua experiência na Trybe.",
+        "category": "Carreira",
+    }
+    mock3 = {
+        "_id": {"$oid": "63ea68e9ee4750d72db4df10"},
+        "url": "https://blog.betrybe.com/tecnologia/next-js/",
+        "title": "Next JS: o que é, para que serve e por que usar?",
+        "timestamp": "27/01/2023",
+        "writer": "Lucas Marchiori",
+        "reading_time": 13,
+        "summary": "Conhecer as principais tecnologias do mercado.",
+        "category": "Tecnologia",
+    }
+    return [mock1, mock2, mock3]
 
 
 def test_reading_plan_group_news():
-    pass
+    mock_assert = {
+        "readable": [
+            {
+                "unfilled_time": 5,
+                "chosen_news": [("computação", 5)],
+            },
+            {
+                "unfilled_time": 1,
+                "chosen_news": [("TrybeTalks — Gaules", 9)],
+            },
+        ],
+        "unreadable": [
+            ("Next JS: o que é, para que serve e por que usar?", 13),
+        ],
+    }
+
+    with patch("tech_news.analyzer.reading_plan.find_news", mock_list):
+        print(ReadingPlanService.group_news_for_available_time(10))
+        assert (
+            ReadingPlanService.group_news_for_available_time(10) == mock_assert
+        )
+        assert ReadingPlanService.group_news_for_available_time(10)[
+            "unreadable"
+        ] == [("Next JS: o que é, para que serve e por que usar?", 13)]
+        with pytest.raises(ValueError):
+            ReadingPlanService.group_news_for_available_time(0)
